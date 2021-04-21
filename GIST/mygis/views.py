a728e8cbbf7fe88c75e5d984a43f405d0aa51fef
Defoult <defou1tako@gmail.com>
2021-04-22 01:07:31 +0300

    added views, html and css added grappeli module

--- GIST/mygis/views.py
+++ GIST/mygis/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from mygis.models import Articles
+
+
+def home(request):
+    # return HttpResponse("Hello world")
+    # return render(request, 'mygis/home.html')
+    articles = Articles.objects.all()
+    context = {
+        'articles': articles
+    }
+    return render(request, 'mygis/home.html', context)
+
+
+def show_articles(request, article_id):
+    article = get_object_or_404(Articles, id=article_id)
+    return render(request, 'mygis/article.html', {'article': article})
+
+
+def about(request):
+    return render(request, 'mygis/about.html')
+
+
+def map_my(request):
+    return render(request, 'mygis/map.html')
